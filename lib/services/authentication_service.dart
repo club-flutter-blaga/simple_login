f5521e9da9a35464ccb95dd0365982f8bf805bd1
Anitei Cristi <christiannitas@gmail.com>
2023-12-21 14:51:46 +0200

    service

---
+++ lib/services/authentication_service.dart
@@ -0,0 +1,16 @@
+class AuthenticationService {
+  static AuthenticationService? _authService;
+
+  static AuthenticationService get authenticationService {
+    _authService ??= AuthenticationService();
+    return _authService!;
+  }
+
+  String? login(String username, String password) {
+    if (username == 'admin' && password == 'password') {
+      return null;
+    } else {
+      return 'Username or password are invalid';
+    }
+  }
+}
